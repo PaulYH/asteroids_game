8160051bf0e157161e8ee4cac6c8a98653d5ea3e
PaulYH <paulhumenny@icloud.com>
2024-11-11 15:47:13 -0500

    Add player movement

--- main.py
+++ main.py
@@ -5,19 +5,16 @@ from player import Player
 def main():
     pygame.init()
 
-    clock = pygame.time.Clock()
-
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    clock = pygame.time.Clock()
     dt = 0
 
-
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while(True):
         for event in pygame.event.get():
@@ -25,6 +22,8 @@ def main():
                 return
 
         screen.fill((0,0,0))
+
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
 
