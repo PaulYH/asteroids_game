900bba03f1a0cad7e3932f65dbc68c44aef445b8
PaulYH <paulhumenny@icloud.com>
2024-11-11 15:39:00 -0500

    Add player sprite

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+
+    clock = pygame.time.Clock()
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
+
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -16,7 +25,10 @@ def main():
                 return
 
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
